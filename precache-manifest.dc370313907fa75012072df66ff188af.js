9ca94f83e7774d4bd3fb15612be2434749265e24
veeresh <veerreshr@gmail.com>
2020-07-22 09:24:40 +0530

    Updates

---
+++ precache-manifest.dc370313907fa75012072df66ff188af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc512a65d21b2c7d9542a4743fa0a35e",
+    "url": "/Random-quote-machine/index.html"
+  },
+  {
+    "revision": "f73ee7c4ba4a4d93865b",
+    "url": "/Random-quote-machine/static/css/main.d45d1639.chunk.css"
+  },
+  {
+    "revision": "95d0ebd11d1417027fbc",
+    "url": "/Random-quote-machine/static/js/2.756294c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Random-quote-machine/static/js/2.756294c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73ee7c4ba4a4d93865b",
+    "url": "/Random-quote-machine/static/js/main.93cd4aca.chunk.js"
+  },
+  {
+    "revision": "589a7555db4e5580baa0",
+    "url": "/Random-quote-machine/static/js/runtime-main.55b56233.js"
+  }
+]);
